672b3717f6c5b613245e482aabe06ad4ac330cd2
kushagra bhavsar <kush_5432@yahoo.com>
2018-02-19 18:04:12 -0500

    modified

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,43 @@
-var movie = ["Minion","Ironman","Titanic","TheHurtLocker","BraveHeart","TheDeparted"];
-var randm = Math.floor(Math.random() * movie.length);
-var randomWord = movie[randm];
-var dash = [];
-var rightGuess = [];
-var wrongGuess = [];
+
+var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+                "P","Q","R","S","T","U","V","W","X","Y","Z"];
+
 var win = 0;
-console.log(randomWord);
-//var uGuess = document.getElementById("words");
-    var userGuess = () =>{
-    for(var i = 0 ; i < randomWord.length ; i++ ) {
-       dash[i] = "_"; 
+var loss = 0;
+var guessRemain = 10;
+var letterGuess = [];
+
+
+document.onkeyup = function(event){
+    var guessed = String.fromCharCode(event.keyCode).toUpperCase();
+    var randm = Math.floor(Math.random() * letters.length);
+    var randomWord = letters[randm];
+    letterGuess.push(guessed);
+
+    if(guessed === randomWord)
+    {
+        win++;
+        guessRemain = 10;
+        letterGuess.length = 0;
+
+    } else if(guessRemain === 0)
+    {
+        loss++;
+        guessRemain = 10;
+        letterGuess.length = 0;
+
+    } else if(guessed !== randomWord)
+    {
+        guessRemain--;
+
     }
+
+    var html= 
+    "<h4>Press any Key to Get Started</h4>" +
+    "<p> Win : " + win + "</p>" +
+    "<p> Loses : " + loss + "</p>" +
+    "<p> Number of Guess Remain : " + guessRemain + "</p>" +
+    "<p> Letters already Guessed : " + letterGuess + "</p>";
+
+    document.querySelector("#psychic").innerHTML = html;
 }
-console.log(userGuess());
-
-    document.getElementById(" ")
-//document.addEventListener("keypress", (event) => {
-  //   var guess = String.fromCharCode(event.keyCode);
-        console.log(guess);
-//  if(randomWord.indexOf(userGuess) > -1)
-//  {
-//      rightGuess.push(userGuess);
-//      dash[randomWord.indexOf(userGuess)] = userGuess;
-        
-//          if(dash.join(" ") === randomWord)
-//          {
-//              win++;
-//          }
-//  }
-//  else{
-//      wrongGuess.push(userGuess);
-//  }
-});
-
